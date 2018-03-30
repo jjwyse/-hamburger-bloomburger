b6a23cb2e08ae17f4eff06f32cff6aa7a541172d
Brian Rothhaar <brian@cloud-elements.com>
2018-03-30 09:12:19 -0400

    :recycle:

--- src/server/db/bin/db-migration.js
+++ src/server/db/bin/db-migration.js
@@ -9,7 +9,7 @@ const DB_DEPLOY_DIRECTORY = `${path.dirname(require.main.filename)}/../sql`;
 const client = require('knex')({
   client: 'pg',
   connection: `postgres://${process.env.BLOOMBURGER_DB_USER}:${process.env.BLOOMBURGER_DB_PASSWORD}@${process.env.BLOOMBURGER_DB_HOST}:${process.env.BLOOMBURGER_DB_PORT}/bloomburger?connect_timeout=10&application_name=bloomburger`,
-  searchPath: 'knex,public',
+  searchPath: ['knex','public'],
 });
 
 const runSqlFile = patchFileName => {
